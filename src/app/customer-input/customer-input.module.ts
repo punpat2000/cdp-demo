0b746f3da74d50b16015dc0e1b577036608c5233
punpat2000 <saint12302@gmail.com>
2020-01-31 19:23:01 +0700

    mat form components
    - imported to the module
    - applied with some input fields

--- src/app/customer-input/customer-input.module.ts
+++ src/app/customer-input/customer-input.module.ts
@@ -4,13 +4,19 @@ import { RouterModule } from '@angular/router';
 import { CustomerInputComponent } from './customer-input.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 
 
 @NgModule({
   declarations: [CustomerInputComponent],
   imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
