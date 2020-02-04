5a1b3e59fdbdb2db3c2044393c0ac0b2bc810a88
Ravipas Wangananont <pestobaimon@gmail.com>
2020-02-04 11:04:17 +0700

    added add tour event

--- src/app/providers/tour.service.ts
+++ src/app/providers/tour.service.ts
@@ -1,25 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Tour } from '../models/tour.model';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable, from, of } from 'rxjs';
+import { Observable, from, of, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TourService {
 
+  private addTourEvent: Subject<string> = new Subject<any>();
+
   constructor(
     private afs: AngularFirestore
   ) { }
 
+  getAddTourEvent(): Observable<any>{
+    return this.addTourEvent.asObservable();
+  }
+
   addTour(t: Tour): void {
-    this.afs.collection(`tours`).doc(t.tourId)
-      .set(t)
-      .then(()=>{
-        console.log('tour added!');
-      }).catch(err => {
-        console.log('error', err);
-      });
+    this.afs.collection('tours').doc(t.tourId).ref.get().then(data => {
+      if (!data) {
+        this.afs.collection(`tours`).doc(t.tourId)
+          .set(t)
+          .then(() => {
+            console.log('tour added!');
+          }).catch(err => {
+            console.log('error', err);
+          });
+      }else{
+        this.addTourEvent.next('tourExists');
+      }
+    });
   }
   getTour(id: string): Observable<Tour> {
     if (this.checkTour(id)) {
